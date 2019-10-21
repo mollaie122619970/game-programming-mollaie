bdd1d87ff24e3fd8c1775edaa4f9b87a01713a7c
mollaie122619970 <56757374+mollaie122619970@users.noreply.github.com>
2019-10-21 23:08:34 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             static void main (string A);
+    
+         Console.WriteLine(welcome friend john
+             have a nice day);}
+        {
+
+            static void main (string A);
+    
+         Console.WriteLine(welcome friend john
+             have a nice day);}
+
+            {
+                int A=g1();
+            int B=g2();
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+        
+        }
+        static int g1();
+    {
+
+    return 5;
+
+}
+
+ static int g2();
+
+{
+ return 6;
+}
+
+    }
+}
+
+{
+int A=g1()*g2();
+int B=(g1()+g2())*2;
+ Console.WriteLine(A);
+ Console.WriteLine(B);
+}
+static int g1();
+{
+return(4);
+}
+static int g2();
+{
+return(6);
+}
+
+
